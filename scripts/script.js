29bec266a4f1a005bfa480dfe19dc35735605746
Fernando Branco <71955135+favanso@users.noreply.github.com>
2022-04-28 18:21:38 -0400

    Chuck Norris API

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+function handleClick() {
+  let url = "https://api.chucknorris.io/jokes/random?category=dev";
+  let output = document.getElementById("output");
+  let message = "";
+  let img = document.getElementById("img");
+
+  img.src = "assets/Img2.jpg";
+  img.setAttribute("width", "254");
+  img.setAttribute("height", "260");
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => {
+      message = json.value;
+
+      output.innerHTML = message;
+    })
+    .catch((e) => console.log(e.message));
+
+
+setTimeout(reset, 10000);
+
+}
+
+function reset() {
+  let img = document.getElementById("img");
+  let output = document.getElementById("output");
+  output.innerText = "";
+  img.src = "assets/download.jpg";
+  img.setAttribute("width", "254");
+  img.setAttribute("height", "198");
+}
