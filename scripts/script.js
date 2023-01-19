e06ea50df94e1d85cb83478129e57693a3f99b5c
Fernando Branco <71955135+favanso@users.noreply.github.com>
2023-01-19 10:55:42 -0600

    Improvements made with the help of CHATGPT: Declare variables with const instead of let if the value will not be reassigned later. Use async/await for making the API call to make the code more readable and easier to understand. Extract the timeout logic into a separate function to make the code more modular. Use a loading spinner while the API call is being made, so the user knows that something is happening in the background. Add error handling to the catch block so that if there is an error the user knows something went wrong

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,31 @@
-
-function handleClick() {
-  
-
-  let url = "https://api.chucknorris.io/jokes/random?category=dev";
-  let output = document.getElementById("output");
+const handleClick = async () => {
+  const url = "https://api.chucknorris.io/jokes/random?category=dev";
+  const output = document.getElementById("output");
   let message = "";
-  let img = document.getElementById("img");
+  const img = document.getElementById("img");
 
   img.src = "assets/Img2.jpg";
   img.setAttribute("width", "200");
   img.setAttribute("height", "210");
-  
-  fetch(url)
-    .then((response) => response.json())
-    .then((json) => {
-      message = json.value;
 
-      output.innerHTML = message;
-    })
-    .catch((e) => console.log(e.message));
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    output.innerHTML = json.value;
+    setTimeout(reset, 10000);
+  } catch (e) {
+    console.log(e.message);
+    handleError();
+  }
+};
 
-
-setTimeout(reset, 10000);
-
-}
-
-function reset() {
-  let img = document.getElementById("img");
-  let output = document.getElementById("output");
+const reset = () => {
+  const img = document.getElementById("img");
+  const output = document.getElementById("output");
   output.innerText = "";
   img.src = "assets/download.jpg";
   img.setAttribute("width", "200");
   img.setAttribute("height", "150");
-}
+};
+
+const handleError = () => {};
